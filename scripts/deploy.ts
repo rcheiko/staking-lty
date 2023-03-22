daec9a8f1139fc3f2228d7bc4ac1322da11b2295
rcheiko <rcheiko@student.42nice.fr>
2023-03-22 09:40:43 +0100

    delete comment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,14 +4,13 @@ async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
-
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const Token = await ethers.getContractFactory("ltyToken"); //Replace with name of your smart contract
+  const Token = await ethers.getContractFactory("ltyToken");
   const token = await Token.deploy();
   console.log("Token address:", token.address);
 
-  const Staking = await ethers.getContractFactory("ltyStaking"); //Replace with name of your smart contract
+  const Staking = await ethers.getContractFactory("ltyStaking");
   const staking = await Staking.deploy(deployer.address, token.address, 1000);
   console.log("Staking address:", staking.address);
 
